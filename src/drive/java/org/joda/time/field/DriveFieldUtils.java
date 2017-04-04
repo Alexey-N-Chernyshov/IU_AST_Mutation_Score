6dfd7b14d495b2782c655e7802128ae2636b12cb
levgenia <evgenia-ya-ya@yandex.ru>
2017-04-04 14:13:47 +0000

    add jpf for DriveFieldUtils.safeMultiplytoInt, safeToInt, getWrappedValue  methods

--- src/drive/java/org/joda/time/field/DriveFieldUtils.java
+++ src/drive/java/org/joda/time/field/DriveFieldUtils.java
@@ -12,9 +12,24 @@ class DriveFieldUtils {
         FieldUtils.safeMultiply(val1, val2);
     }
 
+    public static void test_safeToInt(long value) {
+        FieldUtils.safeToInt(value);
+    }
+
+    public static void test_getWrappedValue(int value, int minValue, int maxValue) {
+        FieldUtils.getWrappedValue(value, minValue, maxValue);
+    }
+
+    public static void test_safeMultiplyToInt(long val1, long val2) {
+        FieldUtils.safeMultiplyToInt(val1, val2);
+    }
+
 
     public static void main(String[] args) {
         test_safeMultiplyInt(1, 2);
         test_safeMultiplyLong(1L, 2L);
+	test_safeToInt(1L);
+	test_getWrappedValue(1,0,2);
+	test_safeMultiplyToInt(1L, 2L);
     }
 }
